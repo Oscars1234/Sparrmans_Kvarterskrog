420f289fa30e1e136c5697e86bfc40bdc6133b7c
oscarsparrman <oscar.sparrman@student.kyh.se>
2021-10-11 11:17:40 +0200

    Initial commit

---
+++ app/src/main/java/edu/kyh/sparrmans_kvarterskrog/Huvudratter.java
@@ -0,0 +1,14 @@
+package edu.kyh.sparrmans_kvarterskrog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Huvudratter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huvudratter);
+    }
+}
